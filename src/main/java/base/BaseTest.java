49f8236a2713c212ec3f56da11e1a56214dbb9fe
shymaxer84 <kaufmandima@gmail.com>
2025-08-29 15:41:49 +0300

    Test1

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import Utilities.Utilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import java.time.Duration;
@@ -13,11 +14,26 @@ public class BaseTest extends Utilities {
     @BeforeMethod
     public void setUP(){
 
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://digital.harel-group.co.il/travel-policy");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utilities.PAGE_WAIT_TIME));
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.get("https://digital.harel-group.co.il/travel-policy");
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utilities.PAGE_WAIT_TIME));
 
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--remote-allow-origins=*");
+
+            String ciEnv = System.getenv("CI");
+            if (ciEnv != null && ciEnv.equalsIgnoreCase("true")) {
+                options.addArguments("--headless=new", "--window-size=1920,1080");
+            } else {
+                options.addArguments("--start-maximized");
+            }
+
+            driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            // 👉 Здесь открывается сайт
+            driver.get("https://digital.harel-group.co.il/travel-policy");
 
     }
     @AfterTest
